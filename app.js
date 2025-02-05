34b5c711d10ea21ee79c9d48439d152eaecb056f
MansourAmal <mansouramal03@gmail.com>
2025-02-05 18:54:42 +0100

    configuration du projet

--- app.js
+++ app.js
@@ -9,15 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const http = require("http"); //1 importer le protocole http
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));  //lier le app avec public
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -38,4 +37,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const server = http.createServer(app); //2 creation de serveur 
+server.listen(5000,()=>{console.log("app is running on port 5000");});
