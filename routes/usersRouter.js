a696049c2c535558b5d167cd69c9f0c0bde1fd11
MansourAmal <mansouramal03@gmail.com>
2025-02-14 20:40:32 +0100

    upload file

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require('../controllers/userController');
+const upload = require('../middelwares/uploadFile');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,5 +14,8 @@ router.post('/addUserAdmin',userController.addUserAdmin);
 router.get('/getAllUsers',userController.getAllUsers); 
 router.get('/getUserById/:id',userController.getUserById); 
 router.delete('/deleteUserById/:id',userController.deleteUserById); 
+
+router.post('/addUserClientWithImg',upload.single("image_user"),userController.addUserClientWithImg); 
+
 
 module.exports = router;
